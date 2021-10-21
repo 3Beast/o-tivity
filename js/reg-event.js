0ddf21a77e6497f3979874143d418d6f447a480f
Eirik <eiriksnedland@gmail.com>
2021-10-21 19:25:29 +0200

    lagte local storage for å submite object fra en html fil til en annen, lager et nytt event hvis siden finner innhold i local storage

---
+++ js/reg-event.js
@@ -0,0 +1,25 @@
+function setup(){
+
+    let btnSubmit = document.getElementById("btn");
+    btnSubmit.addEventListener("click", handleSubmit);
+
+    let inpEvname = document.getElementById("eventname");
+    let inpType = document.getElementById("type");
+    let inpOrg = document.getElementById("org");
+    let inpDate = document.getElementById("date");
+    let inpCap = document.getElementById("capacity");
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        console.log("123");
+       let evname = inpEvname.value;
+        let type = inpType.value;
+        let org = inpOrg.value;
+        let date = inpDate.value;
+        let cap = inpCap.value;
+        let event = {name:evname, type:type, organizer:org, date:date, cap:cap};
+
+        window.localStorage.setItem('event', JSON.stringify(event));
+    }
+}
